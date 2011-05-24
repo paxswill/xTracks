49a43b81f4707e39543d17dee753694818de2b3a
Will Ross <paxswill@gmail.com>
2011-05-23 22:22:11 -0400

    Create a results type for Mixes

---
+++ xTracks/PX8tracksMix.h
@@ -0,0 +1,41 @@
+//
+//  PX8tracksMix.h
+//  xTracks
+//
+//  Created by Will Ross on 5/23/11.
+//  Copyright 2011 Will Ross. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class PX8tracksUser;
+
+@interface PX8tracksMix : NSObject {
+@private
+    NSString *name;
+	BOOL published;
+	NSURL *coverURL;
+	NSInteger idNumber;
+	BOOL liked;
+	PX8tracksUser *user;
+	NSArray *tags;
+	NSUInteger playCount;
+	NSString *mixDescription;
+	NSDate *publishedDate;
+	NSURL *url;
+}
+@property (nonatomic, retain, readonly) NSString *name;
+@property (nonatomic, assign, readonly) BOOL published;
+@property (nonatomic, retain, readonly) NSURL *coverURL;
+@property (nonatomic, assign, readonly) NSInteger idNumber;
+@property (nonatomic, assign, readonly) BOOL liked;
+@property (nonatomic, retain, readonly) PX8tracksUser *user;
+@property (nonatomic, retain, readonly) NSArray *tags;
+@property (nonatomic, assign, readonly) NSUInteger playCount;
+@property (nonatomic, retain, readonly) NSString *mixDescription;
+@property (nonatomic, retain, readonly) NSDate *publishedDate;
+@property (nonatomic, retain, readonly) NSURL *url;
+
+// Initializer From JSON
+
+@end
---
+++ xTracks/PX8tracksMix.m
@@ -0,0 +1,54 @@
+//
+//  PX8tracksMix.m
+//  xTracks
+//
+//  Created by Will Ross on 5/23/11.
+//  Copyright 2011 Will Ross. All rights reserved.
+//
+
+#import "PX8tracksMix.h"
+
+@interface PX8tracksMix()
+@property (nonatomic, retain, readwrite) NSString *name;
+@property (nonatomic, assign, readwrite) BOOL published;
+@property (nonatomic, retain, readwrite) NSURL *coverURL;
+@property (nonatomic, assign, readwrite) NSInteger idNumber;
+@property (nonatomic, assign, readwrite) BOOL liked;
+@property (nonatomic, retain, readwrite) PX8tracksUser *user;
+@property (nonatomic, retain, readwrite) NSArray *tags;
+@property (nonatomic, assign, readwrite) NSUInteger playCount;
+@property (nonatomic, retain, readwrite) NSString *mixDescription;
+@property (nonatomic, retain, readwrite) NSDate *publishedDate;
+@property (nonatomic, retain, readwrite) NSURL *url;
+@end
+
+@implementation PX8tracksMix
+
+@synthesize name;
+@synthesize published;
+@synthesize coverURL;
+@synthesize idNumber;
+@synthesize liked;
+@synthesize user;
+@synthesize tags;
+@synthesize playCount;
+@synthesize mixDescription;
+@synthesize publishedDate;
+@synthesize url;
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
